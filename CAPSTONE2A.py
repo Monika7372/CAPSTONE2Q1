af75a672e914b5db506864261e7f0b3b31bcbfd9
Monika7372 <95435646+Monika7372@users.noreply.github.com>
2021-12-03 00:47:54 +0530

    Add files via upload

---
+++ CAPSTONE2A.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  3 00:43:04 2021
+
+@author: My Laptop
+"""
+
+import turtle as t
+t.fillcolor("RED")
+t.begin_fill()
+for i in range(5):
+        t.forward(100)
+        t.right(144)
+t.end_fill()
